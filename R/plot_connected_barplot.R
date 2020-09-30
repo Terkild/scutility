76e1fa9210a126801e0b8c659dc07baec1dcb663
Terkild <terkild.buus@sund.ku.dk>
2020-09-30 22:31:29 +0200

    Implemented normalize_to to show percentages relative to parent/total populations

--- R/plot_connected_barplot.R
+++ R/plot_connected_barplot.R
@@ -4,6 +4,7 @@
 #' @param group.by Meta data column containing population/cluster assignment (y-axis)
 #' @param split.by Meta data column containing group or sample assignment (x-axis)
 #' @param wrap.by Meta data column to split into multiple independent plots (i.e. patient or tissue information)
+#' @param normalize_to  Data frame containing normalization factor (i.e. total cell count from sample) as first column, split.to (and wrap.by) in following columns. Important that colnames match split.to and wrap.by names
 #' @param cells Vector of rows or cell barcodes to be included in plot.
 #' @param combine Boolean of whether multiple plots (when wrap.by is set) should be combined into a single plot (using cowplot)
 #' @param wrap_add  Integer of how much width should be allocated to y-axis text relative to the with of a bar (for combining plots)
@@ -12,7 +13,7 @@
 #' @importFrom Seurat FetchData
 #' @export
 
-seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wrap.by=NULL, cells=c(), combine=TRUE, wrap_add=0.5, ...){
+seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wrap.by=NULL, normalize_to=NA, cells=c(), combine=TRUE, wrap_add=0.5, ...){
   getData <- Seurat::FetchData(object, vars=c(group.by, split.by, wrap.by))
   colnames(getData)[1:2] <- c("group.by","split.by")
 
@@ -24,7 +25,11 @@ seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wr
     colnames(getData)[3] <- "wrap.by"
 
     dataList <- split(getData, getData$wrap.by)
-    plots <- lapply(dataList, function(x) plot_connected_barplot(population=x$group.by, group=x$split.by, ...) + ggtitle(x$wrap.by[1]))
+    plots <- lapply(dataList, function(x){
+        ## If normalize_to is set, extract data frame related to current "wrap"
+        if(!is.na(normalize_to)) normalize_to <- normalize_to[normalize_to[[wrap.by]] == x$wrap.by[1], c(1,2)]
+        plot_connected_barplot(population=x$group.by, group=x$split.by, normalize_to=normalize_to, ...) + ggtitle(x$wrap.by[1])
+      })
 
     if(combine == TRUE){
       wrapSize <- getData %>%
@@ -38,7 +43,7 @@ seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wr
                          rel_widths=(wrapSize$wrapSize+wrap_add))
     }
   } else {
-    plots <- plot_connected_barplot(population=getData$group.by, group=getData$split.by, ...)
+    plots <- plot_connected_barplot(population=getData$group.by, group=getData$split.by, normalize_to=normalize_to, ...)
   }
 
   return(plots)
@@ -50,6 +55,7 @@ seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wr
 #' @param group vector containing group or sample assignment (x-axis)
 #' @param connected Should bars from same group be connected?
 #' @param bar_width Relative width of bars (integer between 0 and 1)
+#' @param normalize_to Data frame containing normalization factor (i.e. total cell count from sample) as first column and group in the second column.
 #' @param y_value what statistic to plot ("percent" or "count")
 #' @param order should populations be ordered by total count (across groups)
 #' @param colors (named) vector of colors for populations
@@ -58,7 +64,7 @@ seurat_plot_connected_barplot <- function(object, group.by="ident", split.by, wr
 #' @import ggplot2
 #' @export
 
-plot_connected_barplot <- function(population, group, connected=TRUE, bar_width=0.4, y_value="percent", order=FALSE, colors=c(), label=FALSE){
+plot_connected_barplot <- function(population, group, connected=TRUE, bar_width=0.4, normalize_to=NA, y_value="percent", order=FALSE, colors=c(), label=FALSE){
   getData <- data.frame(group=group, population=population)
 
   if(class(group) != "factor") getData$group <- as.factor(getData$group)
@@ -99,9 +105,18 @@ plot_connected_barplot <- function(population, group, connected=TRUE, bar_width=
 
 
   if(y_value == "percent"){
-    plotData <- plotData %>%
-      mutate(pct=populationCount/groupCount*100) %>%
-      mutate(value=pct)
+    if(!is.na(normalize_to)){
+      colnames(normalize_to) <- c("norm", "group")
+
+      plotData <- plotData %>%
+        left_join(normalize_to, by="group") %>%
+        mutate(pct=populationCount/norm*100) %>%
+        mutate(value=pct)
+    } else {
+      plotData <- plotData %>%
+        mutate(pct=populationCount/groupCount*100) %>%
+        mutate(value=pct)
+    }
 
     y_label <- "%"
   } else {
